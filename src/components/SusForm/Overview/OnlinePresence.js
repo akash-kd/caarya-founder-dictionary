fd55eb7f677ab0f3c17bc8d3c7241e2ad6d96f9b
RishiJhawar14 <96866023+RishiJhawar14@users.noreply.github.com>
2023-10-05 13:55:47 +0530

    Added UI Changes

---
+++ src/components/SusForm/Overview/OnlinePresence.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const OnlinePresence = ({ title, presence, placeholder }) => {
+  return (
+    <div className="flex flex-col flex-start gap-4">
+      <h1 className="font-inter text-sm font-semibold text-neutral-500">
+        {title}
+      </h1>
+
+      <div className="flex flex-start justify-between">
+        <div className="flex items-center gap-2">
+          <input type="radio" className="w-4 h-4" />
+          <p className="text-xs font-inter font-light text-neutral-800">
+            Has a {presence}
+          </p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <input type="radio" className="w-4 h-4" />
+          <p className="text-xs font-inter font-light text-neutral-800">
+            Does not have a {presence}
+          </p>
+        </div>
+      </div>
+
+      <input
+        placeholder={placeholder}
+        className="flex px-4 py-3 gap-2 md:min-w-[420px] border border-neutral-300 rounded-lg items-center placeholder:font-light placeholder:text-sm placeholder:font-inter"
+      />
+    </div>
+  );
+};
+
+export default OnlinePresence;
